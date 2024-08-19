bc210b86c22be6671c03acf1d5348263a94ac676
nur-devolvedai <nur@devolvedai.com>
2024-08-19 18:51:27 +0600

    solution page design done

--- src/app/solution/page.tsx
+++ src/app/solution/page.tsx
@@ -3,7 +3,155 @@ import React from 'react'
 export default function Solution() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <div className="pb-16" style={{ fontFamily: '"Lato", sans-serif' }}>
+        {/* Code block starts */}
+
+        <section className="max-w-8xl mx-auto container bg-white pt-16">
+          <div>
+            <div role="contentinfo" className="flex items-center flex-col px-4">
+              {/* <p tabIndex={0} className="focus:outline-none uppercase text-sm text-center text-gray-600 leading-4">in few easy steps</p> */}
+              <h1
+                tabIndex={0}
+                className="focus:outline-none text-4xl lg:text-4xl font-extrabold text-center leading-10 text-gray-800 lg:w-5/12 md:w-9/12 pt-4"
+              >
+                Proposed Solution
+              </h1>
+            </div>
+            <p className='mt-4 px-10 text-lg'>Our startup addresses these challenges by integrating AI and blockchain technologies to build a
+secure, decentralized platform for AI applications. Key components of our solution include:
+</p>
+            <div
+              tabIndex={0}
+              aria-label="group of cards"
+              className="focus:outline-none mt-20 flex flex-wrap justify-center gap-10 px-4"
+            >
+              <div
+                tabIndex={0}
+                aria-label="card 1"
+                className="focus:outline-none flex sm:w-full md:w-5/12 pb-20"
+              >
+                <div className="w-20 h-20 relative mr-5">
+                  <div className="absolute top-0 right-0 bg-indigo-100 rounded w-16 h-16 mt-2 mr-1" />
+                  <div className="absolute text-white bottom-0 left-0 bg-indigo-700 rounded w-16 h-16 flex items-center justify-center mt-2 mr-3">
+                    <img
+                      src="https://tuk-cdn.s3.amazonaws.com/can-uploader/icon_and_text-SVG1.svg"
+                      alt="drawer"
+                    />
+                  </div>
+                </div>
+                <div className="w-10/12">
+                  <h2
+                    tabIndex={0}
+                    className="focus:outline-none text-lg font-bold leading-tight text-gray-800"
+                  >
+                    Building AI Projects with High-Level Security
+                  </h2>
+                  <p
+                    tabIndex={0}
+                    className="focus:outline-none text-base text-gray-600 leading-normal pt-2"
+                  >
+                    We will develop AI projects with a strong emphasis on security, ensuring that all processes and
+data interactions are protected against unauthorized access and breaches.
+
+                  </p>
+                </div>
+              </div>
+              <div
+                tabIndex={0}
+                aria-label="card 2"
+                className="focus:outline-none flex sm:w-full md:w-5/12 pb-20"
+              >
+                <div className="w-20 h-20 relative mr-5">
+                  <div className="absolute top-0 right-0 bg-indigo-100 rounded w-16 h-16 mt-2 mr-1" />
+                  <div className="absolute text-white bottom-0 left-0 bg-indigo-700 rounded w-16 h-16 flex items-center justify-center mt-2 mr-3">
+                    <img
+                      src="https://tuk-cdn.s3.amazonaws.com/can-uploader/icon_and_text-SVG2.svg"
+                      alt="check"
+                    />
+                  </div>
+                </div>
+                <div className="w-10/12">
+                  <h2
+                    tabIndex={0}
+                    className="focus:outline-none text-lg font-semibold leading-tight text-gray-800"
+                  >
+                     Native Cryptocurrency
+                  </h2>
+                  <p
+                    tabIndex={0}
+                    className="focus:outline-none text-base text-gray-600 leading-normal pt-2"
+                  >
+                    Our platform will feature a native cryptocurrency to facilitate secure transactions and incentivize
+network participants. This cryptocurrency will also be used to reward users who contribute to the
+network by providing storage space and computational power.
+                  </p>
+                </div>
+              </div>
+              <div
+                tabIndex={0}
+                aria-label="card 3"
+                className="focus:outline-none flex sm:w-full md:w-5/12 pb-20"
+              >
+                <div className="w-20 h-20 relative mr-5">
+                  <div className="absolute top-0 right-0 bg-indigo-100 rounded w-16 h-16 mt-2 mr-1" />
+                  <div className="absolute text-white bottom-0 left-0 bg-indigo-700 rounded w-16 h-16 flex items-center justify-center mt-2 mr-3">
+                    <img
+                      src="https://tuk-cdn.s3.amazonaws.com/can-uploader/icon_and_text-SVG3.svg"
+                      alt="html tag"
+                    />
+                  </div>
+                </div>
+                <div className="w-10/12">
+                  <h2
+                    tabIndex={0}
+                    className="focus:outline-none text-lg font-semibold leading-tight text-gray-800"
+                  >
+                    Generative AI for Social Media Automation
+                  </h2>
+                  <p
+                    tabIndex={0}
+                    className="focus:outline-none text-base text-gray-600 leading-normal pt-2"
+                  >
+                    We will develop generative AI tools that automate replies and posts on social media, enabling
+users to maintain an active online presence effortlessly. These tools will be designed to
+understand and replicate user behavior while ensuring security and privacy.
+
+                  </p>
+                </div>
+              </div>
+              <div tabIndex={0} aria-label="card 4" className="focus:outline-none flex sm:w-full md:w-5/12 pb-20">
+                <div className="w-20 h-20 relative mr-5">
+                  <div className="absolute top-0 right-0 bg-indigo-100 rounded w-16 h-16 mt-2 mr-1" />
+                  <div className="absolute text-white bottom-0 left-0 bg-indigo-700 rounded w-16 h-16 flex items-center justify-center mt-2 mr-3">
+                    <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/icon_and_text-SVG4.svg" alt="monitor" />
+                  </div>
+                </div>
+                <div className="w-10/12">
+                  <h2 tabIndex={0} className="focus:outline-none text-lg font-semibold leading-tight text-gray-800">Decentralized Data Management                  </h2>
+                  <p tabIndex={0} className="focus:outline-none text-base text-gray-600 leading-normal pt-2">By utilizing decentralized cloud storage, we eliminate single points of failure and provide users
+                  with greater control over their data. This approach enhances security, privacy, and data integrity.</p>
+                </div>
+              </div>
+              <div tabIndex={0} aria-label="card 4" className="focus:outline-none flex sm:w-full md:w-5/12 pb-20">
+                <div className="w-20 h-20 relative mr-5">
+                  <div className="absolute top-0 right-0 bg-indigo-100 rounded w-16 h-16 mt-2 mr-1" />
+                  <div className="absolute text-white bottom-0 left-0 bg-indigo-700 rounded w-16 h-16 flex items-center justify-center mt-2 mr-3">
+                    <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/icon_and_text-SVG4.svg" alt="monitor" />
+                  </div>
+                </div>
+                <div className="w-10/12">
+                  <h2 tabIndex={0} className="focus:outline-none text-lg font-semibold leading-tight text-gray-800">Generative AI for Image Editing</h2>
+                  <p tabIndex={0} className="focus:outline-none text-base text-gray-600 leading-normal pt-2">Our platform will offer advanced generative AI capabilities for image editing, allowing users to
+create and modify images with ease. This feature will be integrated with robust security
+measures to protect user data and intellectual property</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        {/* Code block ends */}
+      </div>
     </div>
   )
 }
