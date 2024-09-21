916674a334c362d2ba3b32f264179ce66c21049d
nureazambd <nur.azam.bd@gmail.com>
2024-09-21 16:21:28 +0600

    new commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 
                     <div className="absolute z-10 top-0 left-0 mx-4 sm:mx-0 mt-36 sm:mt-0 sm:py-20 md:py-28 lg:py-20 xl:py-28 sm:pl-14 flex flex-col sm:justify-start items-start">
                         <h1 className="text-xl sm:text-2xl lg:text-4xl xl:text-6xl font-semibold text-gray-800 sm:w-8/12">Secure Intelligent AI and Blockchain Startup</h1>
-                        <p className="text-base leading-normal text-gray-800 mt-4 sm:mt-5 sm:w-5/12">This white paper presents the vision, strategy, and technological foundation of "Secure Intelligent," our AI and blockchain startup dedicated to delivering highly secure, decentralized AI-driven solutions.</p>
+                        <p className="text-base leading-normal text-gray-800 mt-4 sm:mt-5 sm:w-5/12">This white paper presents the vision, strategy, and technological foundation of Secure Intelligent, our AI and blockchain startup dedicated to delivering highly secure, decentralized AI-driven solutions.</p>
                         {/* <button className="hidden sm:flex bg-gray-800 py-4 px-8 text-base font-medium text-white mt-8 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 hover:bg-gray-700">Explore</button> */}
                     </div>
                     {/* <button className="absolute bottom-0 sm:hidden dark:bg-white dark:text-gray-800 bg-gray-800 py-4 text-base font-medium text-white mt-8 flex justify-center items-center w-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 hover:bg-gray-700">Explore</button> */}
